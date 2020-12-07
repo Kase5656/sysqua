9896d97524dd374d1d73e47b4dc349568510ef86
s_sasaki <sasaki@sqt.co.jp>
2020-12-07 15:55:58 +0900

    受講生検索　修正

---
+++ s-net/WebContent/js/button.js
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+function Education(){
+	var education = document.defaultLink3.Education_cmb
+	var index = education.selectedIndex;
+
+	if(index == 0){
+		btn.disabled = true;
+	}else{
+		btn.disabled = false;
+	}
+}
